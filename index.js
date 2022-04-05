f0084637780c8e9aea0f714a8270399f1ccb5b6c
diego-bellon <difer2085@gmail.com>
2022-04-05 15:40:47 -0500

    fix: Disable gh calls from index.js

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ async function start() {
     const ec2InstanceId = await aws.startEc2Instance(label);
     setOutput(label, ec2InstanceId);
     await aws.waitForInstanceRunning(ec2InstanceId);
-    await gh.waitForRunnerRegistered(label);
+    // await gh.waitForRunnerRegistered(label);
 }
 
 async function stop() {
     await aws.terminateEc2Instance();
-    await gh.removeRunner();
+    // await gh.removeRunner();
 }
 
 (async function () {
