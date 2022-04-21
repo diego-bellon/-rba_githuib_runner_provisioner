902a09b65046ef6dd304cbee81f21548c7a2fde4
Diego Bellon <difer2085@gmail.com>
2022-04-21 14:54:36 -0500

    fix: updating action version to 1.0.31

--- aws.js
+++ aws.js
@@ -14,7 +14,7 @@ function buildUserDataScript(ghtoken, label) {
         'curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
         'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
         'export RUNNER_ALLOW_RUNASROOT=1',
-        `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${token} --labels ${label}`,
+        `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token tokentmp --labels ${label}`,
         './run.sh',
         // '#!/bin/bash',
         // `cd "${config.input.runnerHomeDir}"`,
