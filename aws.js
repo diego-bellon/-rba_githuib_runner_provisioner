7e3f321fede4857e7aaed6aea1702684a5b31681
Diego Bellon <difer2085@gmail.com>
2022-04-21 12:35:41 -0500

    fix: updating action version to 1.0.24

--- aws.js
+++ aws.js
@@ -8,7 +8,7 @@ function buildUserDataScript(ghtoken, label) {
     return [
         '#!/bin/bash',
         'yum install -y jq',
-        'export githubRegistrationToken=$(curl -H \"Authorization: token ${ghtoken}\"   -X POST   -H \"Accept: application/vnd.github.v3+json\" https://api.github.com/repos/${config.githubContext.owner}/${config.githubContext.repo}/actions/runners/registration-token | jq \".token\")',
+        'githubRegistrationToken=$(curl -H \"Authorization: token ${ghtoken}\"   -X POST   -H \"Accept: application/vnd.github.v3+json\" https://api.github.com/repos/${config.githubContext.owner}/${config.githubContext.repo}/actions/runners/registration-token | jq -r .token)',
         'mkdir actions-runner && cd actions-runner',
         'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
         'curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
