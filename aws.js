9e9cd040790b9d89acf38bf6110061fe6e427527
Diego Bellon <difer2085@gmail.com>
2022-04-27 08:50:34 -0500

    fix:Add docker-compose installation to userdata

--- aws.js
+++ aws.js
@@ -10,6 +10,8 @@ function buildUserDataScript(ghtoken, label) {
         'echo "Hello from user-data!"',
         'yum install -y jq',
         'amazon-linux-extras install docker',
+        'curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose',
+        'chmod +x /usr/local/bin/docker-compose',
         'service docker start',
         'usermod -a -G docker ec2-user',
         'yum install -y git',
