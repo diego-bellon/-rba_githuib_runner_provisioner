4f7a57bcff6f21341bd636ce4c7ecc08b0676c39
Diego Bellon <difer2085@gmail.com>
2022-05-09 14:37:15 -0500

    Add remove runner function v1.0.75

--- github.js
+++ github.js
@@ -13,11 +13,11 @@ async function getRunner(label, owner, repo) {
         core.info('Getting runner');
         core.info('Getting owner '+owner);
         core.info('Getting repo '+repo);
-        // const runners = await octokit.paginate('GET /repos/{owner}/{repo}/actions/runners', config.githubContext);
-        const runners = await octokit.rest.actions.listSelfHostedRunnersForRepo({
-            owner,
-            repo,
-        });
+        const runners = await octokit.paginate('GET /repos/'+owner+'/'+repo+'/actions/runners', config.githubContext);
+        // const runners = await octokit.rest.actions.listSelfHostedRunnersForRepo({
+        //     owner,
+        //     repo,
+        // });
         core.info('Runners length'+runners.length);
         core.info('Runners info'+runners[0]);
         const foundRunners = _.filter(runners, {labels: [{name: label}]});
@@ -44,10 +44,10 @@ async function getRegistrationToken() {
 }
 
 async function removeRunnerFromRepo() {
-    core.info('Runner label'+config.input.label);
+    core.info('Runner label '+config.input.label);
     const runner = await getRunner(config.input.label, config.githubContext.owner,config.githubContext.repo);
     const octokit = github.getOctokit(config.input.githubtoken);
-    core.info('Runner id'+runner.id);
+    core.info('Runner id '+runner.id);
     try {
         await octokit.rest.actions.deleteSelfHostedRunnerFromRepo({
             owner: config.input.owner,
