7df1431b83170c8fcdb56fbf2c31997c6728d563
Ivan Castiblanco <ivan.castiblanco@appgate.com>
2023-08-17 11:34:18 -0500

    Add remove runner function v1.0.86-alpha

--- aws.js
+++ aws.js
@@ -26,12 +26,11 @@ function buildUserDataScript(ghtoken, label, runnerVersion) {
         `echo " registration_token=\\$(echo \\"\\$response\\" | jq -r .token)" >> get-runner-token.sh`,
         'echo " sleep 20" >> get-runner-token.sh',
         'echo "done" >> get-runner-token.sh',
-        'echo "export RUNNER_TOKEN=\\$registration_token" >> get-runner-token.sh',
-        'echo "echo \\"TOKEN FINALLY IS \\$RUNNER_TOKEN\\"" >> get-runner-token.sh',
+        'echo "echo \\"TOKEN FINALLY IS \\$registration_token\\"" >> get-runner-token.sh',
+        `echo "./config.sh --unattended --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token $\\registration_token --labels ${label} --name self-hosted-runner-${config.generateUniqueLabel()} --replace" >> get-runner-token.sh`,
+        `echo "./run.sh" >> get-runner-token.sh`,
         'chmod a+x get-runner-token.sh',
-        './get-runner-token.sh',
-        `./config.sh --unattended --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token $RUNNER_TOKEN --labels ${label} --name self-hosted-runner-${config.generateUniqueLabel()} --replace`,
-        './run.sh',
+        './get-runner-token.sh'
     ];
     core.info(userData.join('\n').toString('base64'));
     return userData;
